a44b5a5929d7c4bce7e4289ae9b47ac0f29d58f9
Milani <milaniyoges@gmail.com>
2024-04-05 17:58:49 +0530

    FAQ css

--- src/components/faqs/Faqs.js
+++ src/components/faqs/Faqs.js
@@ -1,45 +1,47 @@
 import React from "react";
+import { useState } from "react";
 import "./Faqs.css";
+
+function Faq({ question, answer }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleFaq = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="faq">
+      <div className="question" onClick={toggleFaq}>
+        <div className="question-text">{question}</div>
+        <div className="button">{isOpen ? "-" : "+"}</div>
+      </div>
+      {isOpen && <div className="answer">{answer}</div>}
+    </div>
+  );
+}
 
 export default function Faqs() {
   return (
     <div className="FAQs">
       <h2>Frequently asked questions</h2>
-      <div className="frame_12527">
-        {/* Question 1 */}
-        <div className="frame_12526">
-          <div className="faqs">What is Lorem Ipsum?</div>
-          <div className="frame_35623">
-            <button>-</button>
-          </div>
-          <div className="frame_35623">
-            <p>
-              Your digital strategy should complement the overall marketing
-              strategy of the company. In online marketing, each component will
-              never work in isolation and every business needs a different mix.
-              We provide a clear concept and strategic overview to find the most
-              efficient model for your business.
-            </p>
-          </div>
-        </div>
-
-        {/* Question 2 */}
-        <div className="frame_12526">
-          <div className="faqs">What is Lorem Ipsum?</div>
-          <div className="frame_35623">
-            <button>-</button>
-          </div>
-          <div className="frame_35623">
-            <p>
-              Your digital strategy should complement the overall marketing
-              strategy of the company. In online marketing, each component will
-              never work in isolation and every business needs a different mix.
-              We provide a clear concept and strategic overview to find the most
-              efficient model for your business.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Faq
+        question="Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?"
+        answer="Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. 
+          Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus 
+          sagittis tortor integer."
+      />
+      <Faq
+        question="Lorem ipsum dolor sit amet consectetur. Tortor scelerisque integer?"
+        answer="Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. 
+          Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus 
+          sagittis tortor integer."
+      />
+      <Faq
+        question="Lorem ipsum dolor sit amet consectetur. Faucibus scelerisque nunc?"
+        answer="Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. 
+          Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus 
+          sagittis tortor integer."
+      />
     </div>
   );
 }
