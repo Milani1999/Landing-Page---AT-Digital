0bc16d763285143e9bb63a3d348372de93fe3fcd
Milani <milaniyoges@gmail.com>
2024-04-05 12:16:25 +0530

    Hero Section

---
+++ src/components/Hero Section/HeroSection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HeroSection.css";
+
+export default function () {
+  return (
+    <div className="HeroSection">
+      <img src="/Hero Image.png" alt="Hero Image" />
+      <div className="frame-497">
+        <p className="p-497">
+          We Crush Your Competitors, Goals, And Sales Records - Without The B.S.
+        </p>
+        <button className="button-497">
+          GET FREE CONSULTATION
+        </button>
+      </div>
+    </div>
+  );
+}
