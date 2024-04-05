f76ea09083a455ef521881c65684d3b901c7b6f6
Milani <milaniyoges@gmail.com>
2024-04-05 22:12:24 +0530

    Netlify error

--- src/components/Hero Section/HeroSection.js
+++ src/components/Hero Section/HeroSection.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./HeroSection.css";
 
-export default function () {
+export default function HeroSection () {
   return (
     <div className="HeroSection">
-      <img src="/HeroImage.png" alt="Hero Image" />
+      <img src="/HeroImage.png" alt="Computer Table" />
       <div className="frame-497">
         <p className="p-497">
           We Crush Your Competitors, Goals, And Sales Records - Without The B.S.
