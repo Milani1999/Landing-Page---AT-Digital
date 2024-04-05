0c7f4a585b1ef4905d663b2d75763faf77463b52
Milani <milaniyoges@gmail.com>
2024-04-05 15:28:35 +0530

    Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroSection from "./components/Hero Section/HeroSection";
 import Frame_498 from "./components/frame 498/Frame_498";
 import Frame_499 from "./components/frame 499/Frame_499";
 import Faqs from "./components/faqs/Faqs";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Frame_498 />
       <Frame_499 />
       <Faqs />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="frame_503">
+        <div className="frame_500">
+          <img src="Logo.png" alt="Logo" />
+          <p>
+            Your goal is our target. Not anything in between. We use online
+            marketing platforms and tools to achieve single objective - your
+            business results.
+          </p>
+        </div>
+        <div className="frame_502">
+          <div className="frame_501">
+            <h5>Our Technologies</h5>
+            <ul type="none">
+              <li>ReactJs</li>
+              <li>Gatsby</li>
+              <li>NextJs</li>
+              <li>NodeJs</li>
+            </ul>
+          </div>
+          <div className="frame_496">
+            <h5>Our Services</h5>
+            <ul type="none">
+              <li>Social media Marketing</li>
+              <li>Web & Mobile App Development</li>
+              <li>Data & Analytics</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="frame_505">
+        <hr />
+        <div className="frame_504">
+          <p>
+            <span className="privacy">Privacy Policy </span>
+            <span className="line">|</span>
+            <span className="terms"> Terms & Conditions</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
