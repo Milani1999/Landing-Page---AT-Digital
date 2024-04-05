3563fbf0a5428e0e6b1d6595c90b392b47b4f94d
Milani <milaniyoges@gmail.com>
2024-04-05 14:27:49 +0530

    FAQ section sketch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavigationBar from "./components/NavigationBar/NavigationBar";
 import HeroSection from "./components/Hero Section/HeroSection";
 import Frame_498 from "./components/frame 498/Frame_498";
 import Frame_499 from "./components/frame 499/Frame_499";
+import Faqs from "./components/faqs/Faqs";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <NavigationBar />
       <HeroSection />
       <Frame_498 />
-      <Frame_499 />{" "}
+      <Frame_499 />
+      <Faqs />
     </div>
   );
 }
---
+++ src/components/faqs/Faqs.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Faqs.css";
+
+export default function Faqs() {
+  return (
+    <div className="FAQs">
+      <h2>Frequently asked questions</h2>
+      <div className="frame_12527">
+        {/* Question 1 */}
+        <div className="frame_12526">
+          <div className="faqs">What is Lorem Ipsum?</div>
+          <div className="frame_35623">
+            <button>-</button>
+          </div>
+          <div className="frame_35623">
+            <p>
+              Your digital strategy should complement the overall marketing
+              strategy of the company. In online marketing, each component will
+              never work in isolation and every business needs a different mix.
+              We provide a clear concept and strategic overview to find the most
+              efficient model for your business.
+            </p>
+          </div>
+        </div>
+
+        {/* Question 2 */}
+        <div className="frame_12526">
+          <div className="faqs">What is Lorem Ipsum?</div>
+          <div className="frame_35623">
+            <button>-</button>
+          </div>
+          <div className="frame_35623">
+            <p>
+              Your digital strategy should complement the overall marketing
+              strategy of the company. In online marketing, each component will
+              never work in isolation and every business needs a different mix.
+              We provide a clear concept and strategic overview to find the most
+              efficient model for your business.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
